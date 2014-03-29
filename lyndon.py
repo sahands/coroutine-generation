b18a659a5f208eb9eb7124631027a7a480aba2a3
Sahand <sahands@gmail.com>
2014-03-29 12:18:01 -0700

    added debruijn cycle generator to the lyndon file.

--- lyndon.py
+++ lyndon.py
@@ -23,9 +23,21 @@ def setup(n):
     return a
 
 
-if __name__ == '__main__':
+def debruijn_cycle(n, k):
+    a = setup(n)
+    primes = []
+    s = []
+    ps = []
+    for a, p in preprimes(a, k, 1, 1):
+        if n % p == 0:
+            primes.append(a[:p])
+            s.extend(a[:p])
+            ps.append(p)
+    return primes, s, ps
+
+
+def test_preprimes(n):
     c = 0
-    n = 4
     a = setup(n)
     for a, p in preprimes(a, 2, 1, 1):
         print(''.join(str(x) for x in a), p, end='')
@@ -36,3 +48,23 @@ if __name__ == '__main__':
             print(" - Necklace", end='')
         print(end='\n')
     print('Count =', c)
+
+
+def find(q, n, k):
+    primes, cycle, p_seq = debruijn_cycle(n, k)
+    cycle_s = ''.join(str(x) for x in cycle)
+    found_index = cycle_s.index(q)
+    index = 0
+    k = 0
+    while index < found_index:
+        index += p_seq[k]
+        k += 1
+    for i in range(k - 2, k + 3):
+        print(''.join(str(x) for x in primes[i]), end=' ')
+    print()
+    print(k - 1)
+
+
+if __name__ == '__main__':
+    find('220121012', 9, 3)
+    find('22012201', 8, 3)
