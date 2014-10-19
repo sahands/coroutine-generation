052fae66f308debfe1689f56ef8fe3994f4286e2
Sahand Saba <sahands@gmail.com>
2014-10-18 20:01:23 -0700

    Fib usage fix.

--- src/basic_examples/fib_usage.py
+++ src/basic_examples/fib_usage.py
@@ -1,3 +1,6 @@
+from fib import fib
+
+
 f = fib()       # Create a new "instance" of the generator coroutine
 print(next(f))  # Prints 1
 print(next(f))  # Prints 1
@@ -5,4 +8,3 @@ print(next(f))  # Prints 2
 print(next(f))  # Prints 3
 print(next(f))  # Prints 5
 print(next(f))  # etc...
-
