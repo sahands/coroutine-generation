c745697bdf3d7425c6c069027a9b0ac8db8b03bd
Sahand Saba <sahands@gmail.com>
2014-10-20 20:11:45 -0700

    Further refactoring.

--- combgen/helpers/permutations/__init__.py
+++ combgen/helpers/permutations/__init__.py
@@ -1,3 +1,4 @@
 from .transpose import transpose
 from .cyclic_shift import left_cyclic_shift
 from .move import move, LEFT, RIGHT
+from .can_move import can_move
--- combgen/linexts/pruesse_ruskey/coroutine/local.py
+++ combgen/linexts/pruesse_ruskey/coroutine/local.py
@@ -1,4 +1,4 @@
-from combgen.helpers.permutations import move, LEFT, RIGHT
+from combgen.helpers.permutations import move, can_move, LEFT, RIGHT
 
 
 MOVED       =  1  # Local coroutine signalling successful transition
@@ -6,19 +6,10 @@ DONE        =  0  # Local coroutine is done and will start over next call
 SWITCH_SIGN = -1  # Local coroutine signalling change of sign
 
 
-def can_move_right(n, poset, pi, inv, x):
-    i = inv[x]
-    if i >= n:
-        return False
-    right = pi[i + 1]
-    return not poset(x, right)
-
-
 def local(n, poset, pi, inv, a, b):
     # local(a, b) is a coroutine responsible for traversing the
     # Hamiltonian cycle for the 2B-poset in which a is before b, followed
     # immediately by the cycle in which b is before a.
-
     def extended_poset(x, y):  # Make sure a < b
         return (x, y) == (a, b) or poset(x, y)
 
@@ -26,8 +17,8 @@ def local(n, poset, pi, inv, a, b):
         mra = mrb = 0
         # If b can not move to the right then we just move a to the right
         # as far as we can.
-        if not can_move_right(n, extended_poset, pi, inv, b):
-            while can_move_right(n, extended_poset, pi, inv, a):
+        if not can_move(n, extended_poset, pi, inv, b, RIGHT):
+            while can_move(n, extended_poset, pi, inv, a, RIGHT):
                 move(pi, inv, a, RIGHT)
                 mra += 1
                 yield MOVED
@@ -39,11 +30,11 @@ def local(n, poset, pi, inv, a, b):
             continue
 
         # Otherwise, let's start traversing the path:
-        while can_move_right(n, extended_poset, pi, inv, b):
+        while can_move(n, extended_poset, pi, inv, b, RIGHT):
             move(pi, inv, b, RIGHT)
             mrb += 1
             yield MOVED
-            while can_move_right(n, extended_poset, pi, inv, a):
+            while can_move(n, extended_poset, pi, inv, a, RIGHT):
                 move(pi, inv, a, RIGHT)
                 mra += 1
                 yield MOVED
