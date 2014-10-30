99defc10dba7e0306da52b2319a0ff181cecd0ec
Sahand Saba <sahands@gmail.com>
2014-10-29 19:56:44 -0700

    Minor changes to chain poset code.

--- combgen/chain_poset_ideals/coroutine/local.py
+++ combgen/chain_poset_ideals/coroutine/local.py
@@ -18,7 +18,6 @@ def bridging_stitch(X, Y):
         yield DONE
 
 
-# Probabyl not an accurate, I don't really know what "hemstiching" is!
 def hem_stitch(X, Y):
     # Produce pattern (X Y | Y X)*
     while True:
@@ -53,6 +52,8 @@ def stitch_coroutines(coroutine_list, stitching_pattern):
 def main():
     n = 5
     a = [0] * n
+    # Represents the chain in which 0 < 1 and 2 < 3 < 4, corresponding to
+    # multiradix numbers with base M[0] = 3 and M[1] = 4
     lead = blind_stitch(hem_stitch(X(a, 1), X(a, 0)),
                        hem_stitch(X(a, 4), hem_stitch(X(a, 3), X(a, 2))))
 
@@ -65,9 +66,37 @@ def main():
             print('--', k, '--')
             k = 0
             c += 1
-            if c > 2:
+            if c > 1:
                 break
 
 
 if __name__ == '__main__':
     main()
+
+# Output:
+# 00000
+# 00001
+# 00011
+# 00111
+# 01111
+# 01111
+# 01111
+# 01111
+# 11111
+# 11110
+# 11100
+# 11000
+# -- 12 --
+# 11000
+# 11000
+# 11000
+# 11000
+# 01000
+# 01001
+# 01011
+# 01111
+# 01111
+# 01111
+# 01111
+# 01111
+# -- 12 --
