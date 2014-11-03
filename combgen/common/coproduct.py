2890049def2340512814840460e128712e27c801
Sahand Saba <sahands@gmail.com>
2014-11-03 15:38:17 -0800

    coproduct is associative, can be done left-associatively, leading to simpler code.

--- combgen/common/coproduct.py
+++ combgen/common/coproduct.py
@@ -2,8 +2,8 @@ from combgen.common import comultiply
 
 
 def coproduct(*Xs):
-    reversed_iterator = reversed(Xs)  # Stitch in reverse order
-    lead = next(reversed_iterator)  # Start with the last coroutine
-    for X in reversed_iterator:
-        lead = comultiply(X, lead)
+    iterator = iter(Xs)
+    lead = next(iterator)
+    for X in iterator:
+        lead = comultiply(lead, X)
     return lead
