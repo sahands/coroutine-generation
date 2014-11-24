db04c285fb5b9af5959a447ea3d4ec684400e6a2
Sahand Saba <sahands@gmail.com>
2014-11-23 20:39:04 -0800

    Better test code for chain posets.

--- combgen/chain_poset_ideals/tests.py
+++ combgen/chain_poset_ideals/tests.py
@@ -1,14 +1,9 @@
 from combgen.common import cosymsum, coproduct
 from combgen.chain_poset_ideals.coroutine import chain_poset_ideals_local as X
-from combgen.chain_poset_ideals.coroutine import gen_all, setup
+from combgen.chain_poset_ideals.coroutine import setup
 
 
-def manual_setup_test():
-    n = 6
-    a = [0] * n
-    lead = coproduct(cosymsum(X(a, 0), X(a, 1)),
-                     X(a, 2),
-                     cosymsum(X(a, 3), X(a, 4), X(a, 5)))
+def run_test(a, lead, repetitions=1):
     k = 0
     c = 0
     while True:
@@ -18,38 +13,43 @@ def manual_setup_test():
             print('--', k, '--')
             k = 0
             c += 1
-            if c > 1:
+            if c > repetitions:
                 break
 
 
-def cyclic_test():
+def tree_test():
+    n = 8
+    a = [0] * n
+    C1 = cosymsum(X(a, 1), X(a, 2))
+    C5 = cosymsum(X(a, 5), X(a, 6))
+    C4 = coproduct(X(a, 4), C5)
+    C3 = cosymsum(X(a, 3), C4)
+    Z = coproduct(C1, C3, X(a, 7))
+    lead = cosymsum(X(a, 0), Z)
+    run_test(a, lead)
+
+
+def manual_setup_test():
     n = 6
-    E = [-1, 1, 2, 5]
-    a, lead = setup(n, E)
-    c = 0
-    while True:
-        print(''.join(str(x) for x in a))
-        if not next(lead):
-            print('---')
-            c += 1
-            if c > 2:
-                break
+    a = [0] * n
+    lead = coproduct(cosymsum(X(a, 0), X(a, 1)),
+                     X(a, 2),
+                     cosymsum(X(a, 3), X(a, 4), X(a, 5)))
+    run_test(a, lead)
 
 
 def auto_setup_test():
     # The "heads" of the chains 0 < *1*, *2* stand-alone, and 3 < 4 < *5*
     n = 6
     E = [-1, 1, 2, 5]
-    A = list(a[:] for a in gen_all(n, E))
-    for a in A:
-        print(''.join(str(x) for x in a))
-    print('--', len(A), '--')
+    a, lead = setup(n, E)
+    run_test(a, lead)
 
 
 def main():
+    tree_test()
     manual_setup_test()
     auto_setup_test()
-    cyclic_test()
 
 
 if __name__ == '__main__':
